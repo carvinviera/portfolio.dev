c0c6f8b18787dbfbc707d2e19b0f323cf03123aa
carvinviera <carvinviera@gmail.com>
2025-10-21 14:06:03 +0200

    build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://porfolio.dev/',
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    }
+  }
 })
