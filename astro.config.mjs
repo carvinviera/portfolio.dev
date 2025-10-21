c97a40403387f237492e96b5fafcb57d008ca9d1
carvinviera <carvinviera@gmail.com>
2025-10-21 18:55:10 +0200

    sensitive

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import { fileURLToPath } from "url";
+import netlify from "@astrojs/netlify"; // <--- 1. IMPORTAR
 
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: "https://porfolio.dev/",
+  output: 'server', // <--- 2. AÑADIR ESTA LÍNEA
+  integrations: [
+    tailwind(), 
+    robotsTxt(),
+    netlify() // <--- 3. AÑADIR EL ADAPTADOR
+  ],
+  site: "https://carvinviera-dev-dd6e0c.netlify.app/",
   vite: {
     resolve: {
       alias: {
